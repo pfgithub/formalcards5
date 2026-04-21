3b750d74f19ab2492222e907816f724bfdc93a29
pfg <pfg@pfg.pw>
2026-04-20 17:51:26 -0700

    impl pass, can_play, play_card

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,37 @@ use rand::seq::SliceRandom;
 
 fn main() {
     println!("Hello, world!");
+    let mut input = Input {
+        deck: Pile::<Card>::new(),
+        players: Ring::<Player>::new(),
+    };
+    for suit in vec![CardSuit::Clubs, CardSuit::Spades, CardSuit::Hearts, CardSuit::Diamonds] {
+        for number in vec![
+            CardNumber::A,
+            CardNumber::_2,
+            CardNumber::_3,
+            CardNumber::_4,
+            CardNumber::_5,
+            CardNumber::_6,
+            CardNumber::_7,
+            CardNumber::_8,
+            CardNumber::_9,
+            CardNumber::_10,
+            CardNumber::J,
+            CardNumber::Q,
+            CardNumber::K,
+        ] {
+            input.deck.add(vec![Card {suit, number}]);
+        }
+    }
+    input.players.add(vec![
+        Player{id: 0},
+        Player{id: 1},
+        Player{id: 2},
+        Player{id: 3},
+    ]);
+    let output = game(&mut input).expect("todo error");
+    println!("Output: {:?}", output);
 }
 
 macro_rules! action_screen {
@@ -12,7 +43,7 @@ macro_rules! action_screen {
         $key:ident ( $($value:tt)* ),
     )* }) => {
         $(action_screen!(@typedef $($value)*);)*
-        enum $name {
+        pub enum $name {
             $(
                 $key (action_screen!(@typename $($value)*)),
             )*
@@ -58,9 +89,9 @@ macro_rules! action_screen {
         $key:ident: ($($value:tt)*),
     )* }) => {
         $(action_screen!(@typedef $($value)*);)*
-        struct $name {
+        pub struct $name {
             $(
-                $key: action_screen!(@typename $($value)*),
+                pub $key: action_screen!(@typename $($value)*),
             )*
         }
     };
@@ -120,9 +151,15 @@ macro_rules! action_screen {
         }
     };
 
+    // @raw
+    (@typedef @raw $rest:expr) => {};
+    (@typename @raw $rest:expr) => {ActionScreenResult};
+    (@screen @raw $rest:expr) => {$rest};
+    (@resolve ($resolve:expr) @raw $rest:expr) => {$resolve};
+
     // @never
     (@typedef @never) => {};
-    (@typename @never) => {()};
+    (@typename @never) => {!};
     (@screen @never) => {ActionScreen::Enum(vec![])};
     (@resolve ($resolve:expr) @never) => {panic!("unreachable")};
 }
@@ -134,6 +171,18 @@ macro_rules! wait_action_screen {
         action_screen!(@typedef $($rest)*);
         let $name = action_screen!(@resolve (raw) $($rest)*);
     };
+}
+
+macro_rules! make_action_screen {
+    ($args:tt $($rest:tt)*) => {
+        use super::*;
+        action_screen!(@typedef $($rest)*);
+        pub fn wait $args -> action_screen!(@typename $($rest)*) {
+            let screen = action_screen!(@screen $($rest)*);
+            let encoded = wait_action_screen(screen);
+            action_screen!(@resolve (encoded) $($rest)*)
+        }
+    }
 }
 
 fn game(input: &mut Input) -> Result<Output, Error> {
@@ -174,30 +223,86 @@ fn game(input: &mut Input) -> Result<Output, Error> {
     state.turn = *state.circle.clockwise_after(state.turn).first().expect(">=1 player");
 
     loop {
-
-        wait_action_screen!(let action = @choose Action {
-            Pass(@record ActionPass {
-                actor: (@actor Some(vec![state.turn])),
-            }),
-            Play(@record ActionPlay {
-                actor: (@actor Some(vec![state.turn])),
-                card: (@enum<Card> state.hand(state.turn).contents),
-            }),
-        });
+        mod base_action {
+            make_action_screen!((turn: Player, playable: Vec<Card>) @choose Action {
+                Pass(@record ActionPass {
+                    _actor: (@actor Some(vec![turn])),
+                }),
+                Play(@record ActionPlay {
+                    _actor: (@actor Some(vec![turn])),
+                    card: (@enum<Card> playable),
+                }),
+            });
+        }
+        let hand_cont = state.hand(state.turn).contents.clone();
+        let allowed_cards = hand_cont.iter().filter_map(|card| {
+            let card: Card = *card;
+            if can_play(&state, card) {Some(card)} else {None}           
+        }).collect();
+        let action = base_action::wait(state.turn, allowed_cards);
 
         match action {
-            Action::Pass(_pass) => {
-                panic!("TODO: impl pass");
-            },
-            Action::Play(play) => {
+            base_action::Action::Play(play) => {
                 play_card(&mut state, play.card);
+            },
+            base_action::Action::Pass(_) => {
+                if let Some(drawn_card) = draw_or_reshuffle(&mut state) {
+                    wait_action_screen!(let action = @choose InnerAction {
+                        Pass(@record InnerActionPass {
+                            _actor: (@actor Some(vec![state.turn])),
+                        }),
+                        Play(@record InnerActionPlay {
+                            _actor: (@actor Some( if can_play(&state, drawn_card) {vec![state.turn]} else {vec![]})),
+                        }),
+                    });
+                    match action {
+                        InnerAction::Pass(_) => {
+                            state.hand(state.turn).add(vec![drawn_card]);
+                        },
+                        InnerAction::Play(_) => {
+                            play_card(&mut state, drawn_card);
+                        },
+                    };
+                }
             },
         }
     }
 }
 fn play_card(state: &mut State, card: Card) -> () {
+    state.eight_suit = None;
+    state.draw_pile.add(vec![card]);
+    if state.hand(state.turn).contents.is_empty() {
+        // TODO: win game
+    }
+    if card.number == CardNumber::_8 {
+        wait_action_screen!(let action = @record Action {
+            _actor: (@actor Some(vec![state.turn])),
+            suit: (@enum<CardSuit> vec![CardSuit::Diamonds, CardSuit::Spades, CardSuit::Hearts, CardSuit::Clubs]),
+        });
+        state.eight_suit = Some(action.suit);
+    }
+}
+fn can_play(state: &State, card: Card) -> bool {
+    if card.number == CardNumber::_8 {return true};
+    if let Some(eight_suit) = state.eight_suit {return card.suit == eight_suit};
+    let top = state.draw_pile.peek_top().expect("there is always a card on top of discard");
+    return card.number == top.number || card.suit == top.suit;
+}
+fn draw_or_reshuffle(state: &mut State) -> Option<Card> {
+    if let Some(t) = state.draw_pile.take_top() {
+        return Some(t);
+    }
+    let prev = state.discard_pile.take_top();
+    state.draw_pile.add(state.discard_pile.take_all());
+    if let Some(prev) = prev {
+        state.discard_pile.add(vec![prev]);
+    }
+    state.draw_pile.take_top()
 }
 
+trait ActionScreenOption {}
+
+#[derive(Debug)]
 enum ActionScreen {
     Choose(Vec<(&'static str, ActionScreen)>),
     Record(Vec<(&'static str, ActionScreen)>),
@@ -213,9 +318,10 @@ enum ActionScreenResult {
 // wonder if we can make it typesafe somehow like the js one
 // or better given that we can use numbers instead of string keys
 fn wait_action_screen(screen: ActionScreen) -> ActionScreenResult {
-    panic!("todo");
+    panic!("todo: wait_action_screen: {:?}", screen);
 }
 
+#[derive(Debug)]
 enum Error {
     Cards,
 }
@@ -244,6 +350,7 @@ struct Input {
     deck: Pile<Card>,
     players: Ring<Player>,
 }
+#[derive(Debug)]
 struct Output {
     winner: Player,
 }
@@ -265,6 +372,9 @@ impl<T: Copy> Pile<T> {
     fn shuffle(&mut self) -> () {
         let mut rng = rand::rng();
         self.contents.shuffle(&mut rng);
+    }
+    fn peek_top(&self) -> Option<T> {
+        self.contents.last().copied()
     }
     fn take_top(&mut self) -> Option<T> {
         self.contents.pop()
