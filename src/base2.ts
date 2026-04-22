c285767395d0338121da33f32bbc0e35406e3be3
pfg <pfg@pfg.pw>
2026-04-22 14:51:54 -0700

    base2 attempt. what's even the difference? maybe this time it can be serialized into views automatically? do we want that?

--- src/base2.ts
+++ src/base2.ts
@@ -1,9 +1,122 @@
 import { jsShuffle } from "./base";
 
+
+const hand = defineEstablishment<Pile<Card>>("hand");
+const draw_pile = defineEstablishment<Pile<Card>>("draw pile");
+const discard_pile = defineEstablishment<Pile<Card>>("discard pile");
 function main(table: Group, player_ring: Ring<Player>, deck: Pile<Card>) {
+    const dealer = defineRemember<Player>("dealer", player_ring.random());
 
-    const draw_pile = deck.takeSub().shuffle().establish(table, "draw pile");
+    for (const player of player_ring.clockwise(dealer.value)) {
+        new Hand<Card>(player).establish(player, hand);
+    }
 
+    deck.takeSub().shuffle().establish(table, draw_pile);
+    for (let i = 0; i < 7; i += 1) {
+        for (const player of player_ring.clockwise(dealer.value)) {
+            player.get(hand).add(table.get(draw_pile).takeSub(-1).items);
+        }
+    }
+    table.get(draw_pile).takeSub(-1).flip().establish(table, discard_pile);
+
+    const turn = defineRemember<Player>("turn", player_ring.clockwise(dealer.value)[1]!);
+    const eight_suit = defineRemember<CardSuit | undefined>("eight suit", undefined);
+
+    while (true) {
+        type ActionRes = {kind: "play", card: Card} | {kind: "draw"};
+        const action: ActionRes = waitFor((player): WaitItem<ActionRes>[] => {
+            if (player !== turn.value) return [];
+            return [
+                ...player.get(hand).items.filter(card => canPlay(card.viewAs(player), table.get(discard_pile), eight_suit.value)).map((card): WaitItem<ActionRes> => {
+                    return {
+                        trigger: {kind: "move", obj: card, to: table.get(discard_pile)},
+                        action: {kind: "play", card},
+                    };
+                }),
+                {
+                    trigger: {kind: "move", obj: table.get(draw_pile).peekSub(-1)[0]!, to: player.get(hand)},
+                    action: {kind: "draw"},
+                },
+            ];
+        });
+        if (action.kind === "play") {
+            playCard(table, turn.value, action.card, turn.value.get(hand), table.get(discard_pile), eight_suit);
+        } else if (action.kind === "draw") {
+            type ActionRes2 = "play" | "pass";
+            const drawn = table.get(draw_pile).takeSub(-1).face(turn.value).items[0]!;
+            if (table.get(draw_pile).empty()) {
+                table.get(draw_pile).add(table.get(discard_pile).takeSub(0, -1).flip().shuffle().items);
+            }
+            const action: ActionRes2 = waitFor((player): WaitItem<ActionRes2>[] => {
+                if (player !== turn.value) return [];
+                return [
+                    ...canPlay(drawn.viewAs(turn.value), table.get(discard_pile), eight_suit.value) ? [{
+                        trigger: {kind: "move", obj: drawn, to: table.get(discard_pile)},
+                        action: "play",
+                    } satisfies WaitItem<ActionRes2> as WaitItem<ActionRes2>] : [],
+                    {
+                        trigger: {kind: "move", obj: drawn, to: player.get(hand)},
+                        action: "pass",
+                    },
+                ];
+            });
+            if (action === "play") {
+                playCard(table, turn.value, drawn, turn.value.get(hand), table.get(discard_pile), eight_suit);
+            } else if (action === "pass") {
+                turn.value.get(hand).add([drawn]);
+            }
+        }
+    }
+}
+
+function canPlay(card: CardView, discard_pile: Pile<Card>, eight_suit: CardSuit | undefined): boolean {
+    if (card.number === "8") return true;
+    if (eight_suit != null) return card.suit === eight_suit;
+    const top = discard_pile.peekSub(-1)[0]!.viewAs("*");
+    return card.number === top.number || card.suit === top.suit;
+}
+function playCard(table: Object, turn: Player, card: Card, hand: Pile<Card>, discard_pile: Pile<Card>, eight_suit: Remember<CardSuit | undefined>) {
+    discard_pile.add([card.take().flip()]);
+    if (hand.empty()) {
+        throw new WinError(turn);
+    }
+    eight_suit.value = undefined;
+    if (card.viewAs("*").number === "8") { // alt: card.performEffect(state)
+        eight_suit.value = waitFor((player): WaitItem<CardSuit>[] => {
+            if (player !== turn) return [];
+            return card_suits.map((suit): WaitItem<CardSuit> => ({
+                trigger: {kind: "announce", msg: suit},
+                action: suit,
+            }));
+        });
+    }
+}
+class WinError extends Error {constructor(public player: Player) {super("win")}}
+
+type Establishment<T extends Object> = {name: string, est_sym: symbol, _t: T};
+function defineEstablishment<T extends Object>(name: string): Establishment<NoInfer<T>> {
+    return {name, est_sym: Symbol(), _t: 0 as any};
+}
+
+type Remember<T> = {name: string, rem_sym: symbol, value: T};
+function defineRemember<T>(name: string, value: NoInfer<T>): Remember<NoInfer<T>> {
+    return {name, rem_sym: Symbol(), value};
+}
+
+type WaitTrigger = {
+    kind: "move",
+    obj: Object,
+    to: Object,
+} | {
+    kind: "announce",
+    msg: string,
+};
+type WaitItem<T> = {
+    trigger: WaitTrigger,
+    action: T,
+};
+function waitFor<T>(cb: (player: Player) => WaitItem<T>[]): NoInfer<T> {
+    throw new Error("TODO wait");
 }
 
 /*
@@ -104,25 +217,48 @@ class Object {
         item.owner = undefined;
     }
 
-    establish(group: Group, name: string) {
+    establish<T extends Object>(this: NoInfer<T>, group: Group, name: Establishment<T>) {
         group.add(name, this);
+        return this;
     }
+    flip() {return this}
 }
 class Group extends Object {
-    named: Map<Object, string> = new Map();
-    add(name: string, item: Object) {
+    named: Map<Object, Establishment<Object>> = new Map();
+    reverse: Map<symbol, Object> = new Map();
+    add<T extends Object>(name: Establishment<T>, item: NoInfer<T>) {
         this.own(item);
         this.named.set(item, name);
+        this.reverse.set(name.est_sym, item);
+    }
+    get<T extends Object>(name: Establishment<T>): NoInfer<T> {
+        return this.reverse.get(name.est_sym) as T;
     }
     override disown(item: Object): void {
         super.disown(item);
+        this.reverse.delete(this.named.get(item)!.est_sym);
         this.named.delete(item);
     }
 }
 class Ring<T extends Object> extends Object {
+    items: T[] = [];
+    random(): T {
+        return this.items[Math.random() * this.items.length |0]!;
+    }
+    clockwise(from: T): T[] {
+        const i = this.items.indexOf(from);
+        return [...this.items.slice(i), ...this.items.slice(0, i)];
+    }
 }
+type FacingTarget = Player | "table";
 class Pile<T extends Object> extends Object {
     items: T[] = [];
+    facing_target: FacingTarget;
+
+    constructor(facing_target: FacingTarget) {
+        super();
+        this.facing_target = facing_target;
+    }
 
     shuffle() {
         jsShuffle(this.items);
@@ -131,10 +267,22 @@ class Pile<T extends Object> extends Object {
         for (let i = 0; i < ids.length; i++) this.items[i]!.id = i;
         return this; // eew
     }
+    override flip() {
+        this.items.reverse();
+        for (const item of this.items) item.flip();
+        return super.flip();
+    }
+    peekSub(start: number = 0, end: number = this.items.length): T[] {
+        return this.items.slice(start, end);
+    }
     takeSub(start: number = 0, end: number = this.items.length): Pile<T> {
-        const res = new Pile<T>();
-        res.add(this.items.slice(start, end));
+        const res = new Pile<T>(this.facing_target);
+        res.add(this.peekSub(start, end));
         return res;
+    }
+    face(facing_target: FacingTarget) {
+        this.facing_target = facing_target;
+        return this;
     }
     add(items: T[]) {
         for (const it of items) {
@@ -142,25 +290,52 @@ class Pile<T extends Object> extends Object {
             this.items.push(it);
         }
     }
+    empty(): boolean {
+        return this.items.length === 0;
+    }
     override disown(item: Object): void {
         super.disown(item);
         this.items.splice(this.items.indexOf(item as T), 1);
     }
 }
 
+class Hand<T extends Object> extends Pile<T> {
+
+}
+
 class Player extends Group {}
 
 class Card extends Object {
-    facing: "sky" | "table" | Player = "table";
+    visible_to_all: boolean = true;
 
-    viewAs(perspective: Player): {
-        suit: CardSuit,
-        number: CardNumber,
-    } {
-        // if the player can't view the card, error
-        throw new Error("TODO Card.view");
+    override flip() {
+        this.visible_to_all = !this.visible_to_all;
+        return super.flip();
+    }
+
+    take(): Card {
+        this.owner?.disown(this);
+        return this;
+    }
+
+    viewAs(perspective: Player | "*"): CardView {
+        if (!this.owner) throw new Error("card has no owner, todo");
+        if (!(this.owner instanceof Pile)) throw new Error("!owner pile todo");
+        if (this.owner.facing_target === perspective) {
+            // facing you
+            if (this.visible_to_all) throw new Error("you can't see this card, it's facing away from you");
+            throw new Error("TODO: get card data from view map");
+        } else {
+            // facing table
+            if (this.visible_to_all) throw new Error("TODO: get card data from view map");
+            throw new Error("you can't see this card");
+        }
     }
 }
+type CardView = {
+    suit: CardSuit,
+    number: CardNumber,
+};
 
 export type CardSuit = "D" | "H" | "C" | "S";
 const card_suits: CardSuit[] = ["D", "H", "C", "S"];
